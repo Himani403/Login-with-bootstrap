b4a1852b1b63653a9c5ddb30d7893902c784b89a
Himani Patel <hrp03193@gmail.com>
2021-02-11 22:45:59 -0500

    Frist commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
-
+import { analyzeAndValidateNgModules } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import{ FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'mypro1';
+  title = 'angulartoastr';
+  showModal: any;
+  registerForm: any;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) { }
+  show()
+  {
+    this.showModal = true; 
+  }
+ 
+  hide()
+  {
+    this.showModal = false;
+  }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+}
+
+get f() { return this.registerForm.controls; }
+onSubmit() {
+    this.submitted = true;
+    
+    if (this.registerForm.invalid) {
+        return;
+    }
+    if(this.submitted)
+    {
+      this.showModal = false;
+    }
+   
+}
+  
 }
